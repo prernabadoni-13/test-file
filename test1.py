bc76f5519e5ed254057539380142983e1149634d
Prerna Badoni <prernabadoni@Prernas-MacBook-Air.local>
2026-03-19 14:09:31 +0530

    initial commit

---
+++ test1.py
@@ -0,0 +1,8 @@
+def divide(a, b)
+    return a / b
+
+x = 10
+y = 0
+
+result = divide(x, y)
+print("Result is: " + result)
---
+++ test2.java
@@ -0,0 +1,10 @@
+public class Test {
+    public static void main(String[] args) {
+        int a = 10
+        int b = 0;
+
+        int result = a / b;
+
+        System.out.println("Result: " + result)
+    }
+}
