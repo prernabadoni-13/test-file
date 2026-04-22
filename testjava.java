514dfad338c49211ca0ee81c1dc46edd786decfb
prernabadoni-13 <139848325+prernabadoni-13@users.noreply.github.com>
2026-04-22 11:52:38 +0530

    Add Test class with main method

---
+++ testjava.java
@@ -0,0 +1,11 @@
+public class Test {
+    public static void main(String[] args) {
+        int a = 10
+        int b = 0;
+
+        int result = a / b;
+
+        System.out.println("Result: " + result)
+        System.out.println("result 2");
+    }
+}
